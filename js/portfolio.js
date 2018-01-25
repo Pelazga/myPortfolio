67b537375164caec1574cf0b91aa0899a9e5c666
Dmyterko <pelazga@gmail.com>
2018-01-25 22:07:09 +0200

    исправлена ошибка в цикле for

--- js/portfolio.js
+++ js/portfolio.js
@@ -10,7 +10,7 @@ var gallery = document.getElementsByClassName("gallery-section");
 
 function removeActivatingClass (arr, className, arr2, className2){
     function removeClass (arr, className){
-        for (i=0; i<=arr.length; i++) {
+        for (i=0; i<=arr.length-1; i++) {
             if (arr[i].classList.contains(className)){
                 arr[i].classList.remove(className)
             }
@@ -42,56 +42,3 @@ textLink.onclick = function() {
         textLink.classList.add("active");
     }, 250);
 };
-
-
-
-// function projectDeactivate() {
-//     if (projectGall.classList.contains("gallery-section-active")){
-//         projectGall.classList.remove("gallery-section-active");
-//     };
-//     if (projectLink.classList.contains("active")){
-//         projectLink.classList.remove("active");
-//     };
-// };
-// function translationDeactivate() {
-//     if (translationGall.classList.contains("gallery-section-active")){
-//         translationGall.classList.remove("gallery-section-active");
-//     };
-//     if (translationLink.classList.contains("active")){
-//         translationLink.classList.remove("active");
-//     };
-// };
-// function textDeactivate() {
-//     if (textGall.classList.contains("gallery-section-active")){
-//         textGall.classList.remove("gallery-section-active");
-//     };
-//     if (textLink.classList.contains("active")){
-//         textLink.classList.remove("active");
-//     };
-// };
-
-
-// projectLink.onclick = function() {
-//     translationDeactivate();
-//     textDeactivate();
-//     setTimeout(function() {
-//         projectGall.classList.add("gallery-section-active");
-//         projectLink.classList.add("active");
-//     }, 250);
-// };
-// translationLink.onclick = function() {
-//     textDeactivate();
-//     projectDeactivate();
-//     setTimeout(function() {
-//         translationGall.classList.add("gallery-section-active");
-//         translationLink.classList.add("active");
-//     }, 250);
-// };
-// textLink.onclick = function() {
-//     translationDeactivate();
-//     projectDeactivate();
-//     setTimeout(function() {
-//         textGall.classList.add("gallery-section-active");
-//         textLink.classList.add("active");
-//     }, 250);
-// };
