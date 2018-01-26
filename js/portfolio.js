4aa2f4549ce2a15c7c865b07e2b99ad5837b5708
Dmyterko <pelazga@gmail.com>
2018-01-26 12:52:17 +0200

    Пересмотрена функция removeActivatingClass, убраны принимающие параметры.

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,7 +8,7 @@ var textGall = document.getElementById("seoTextProjects");
 var tabs = document.getElementsByClassName("tabb");
 var gallery = document.getElementsByClassName("gallery-section");
 
-function removeActivatingClass (arrOfLinks, linkClassName, arrOfDivs, divClassName){
+function removeActivatingClass (){
     function removeClass (arr, className){
         for (i=0; i<=arr.length-1; i++) {
             if (arr[i].classList.contains(className)){
@@ -16,26 +16,26 @@ function removeActivatingClass (arrOfLinks, linkClassName, arrOfDivs, divClassNa
             }
         }
     };
-    removeClass(arr, className);
-    removeClass(arr2, className2);
+    removeClass(tabs, "active");
+    removeClass(gallery, "gallery-section-active");
 };
 
 projectLink.onclick = function() {
-    removeActivatingClass(tabs, "active", gallery, "gallery-section-active");
+    removeActivatingClass();
     setTimeout(function() {
         projectLink.classList.add("active");
         projectGall.classList.add("gallery-section-active");
     }, 250);
 };
 translationLink.onclick = function() {
-    removeActivatingClass(tabs, "active", gallery, "gallery-section-active");
+    removeActivatingClass();
     setTimeout(function() {
         translationGall.classList.add("gallery-section-active");
         translationLink.classList.add("active");
     }, 250);
 };
 textLink.onclick = function() {
-    removeActivatingClass(tabs, "active", gallery, "gallery-section-active");
+    removeActivatingClass();
     setTimeout(function() {
         textGall.classList.add("gallery-section-active");
         textLink.classList.add("active");
